2e420130a3287552aeaabc1e7f6c6da4eb02cf3d
emoalb <e_alb@abv.bg>
2021-07-17 17:33:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {useEffect,useState} from 'react';
+
+const App = () => {
+
+  const APP_ID = '998d3865';
+  const APP_KEY = '3b3cf17260c6f7537b84dd8dc3a786cb';
+  useEffect(()=>{
+  getRecipies("chocolate")
+  },[])
+  const getRecipies = async (recipie) => {
+  let request_string =  `https://api.edamam.com/api/recipes/v2?type=public&q=${recipie}&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const response = await fetch(request_string);
+  const data  = await response.json();
+  console.log(data);
+}
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type = "button">Search</button>
+      </form>
     </div>
   );
 }
